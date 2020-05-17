3535355dd8505c85dab700af80bcbe9c0fe1abc0
Roman Mosin <mosinra90@yandex.ru>
2020-05-17 15:36:52 +0500

    hw 1 ИСПРАВИЛ

--- src/Day/Main.java
+++ src/Day/Main.java
@@ -2,11 +2,10 @@ package Day;
 
 public class Main {
     public static void main(String[] args) {
-        DayOfWeek time = DayOfWeek.FRIDAY;
 
-        for (DayOfWeek d: DayOfWeek.values() ) {
-            if (d.getTime()!=0)
-            System.out.println(d + " до конца рабочей недели - " + d.getTime() + " часа(ов)");
+        for (DayOfWeek d : DayOfWeek.values()) {
+            if (d.getTime() != 0)
+                System.out.println(d + " до конца рабочей недели - " + d.getTime() + " часа(ов)");
             else {
                 System.out.println(d + " - Сегодня выходной");
             }
